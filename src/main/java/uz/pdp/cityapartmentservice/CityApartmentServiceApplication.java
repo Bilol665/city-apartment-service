156f0bfcbcf8549255913e35728cf93be7310599
Bilol665 <bilolsultan665@gmail.com>
2023-07-31 15:54:12 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/cityapartmentservice/CityApartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.cityapartmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityApartmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityApartmentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/cityapartmentservice/CityApartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.cityapartmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityApartmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
